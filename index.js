bb335f0379ad3f6b57d60c6dbe5e27c7f3f65e6c
Ryan Engel <Rcengel@comcast.net>
2023-03-05 16:37:53 -0800

    wanted to add a few more examples

--- index.js
+++ index.js
@@ -84,3 +84,75 @@ function save() {
 //welcomeEl.innerText = greeting + name 
 
 //welcomeEl.innerText += " hi"
+
+//concadinating variables
+const firstName = "Ryan"
+const lastName = " Engel"
+let fullName = firstName + lastName
+
+console.log(fullName)
+
+//function to call in console
+let name = "Linda"
+let greeting = "Hi there"
+
+function greetLinda(){
+    console.log(greeting + ", " + name + "!")
+}
+greetLinda()
+
+//onclick function
+function nameGreeting() {
+    let call = greeting + ", " + name + "!"
+    console.log(call)
+}
+
+//incrementing and decrementing variables
+let myPoints = 3
+
+function add3Points() {
+    myPoints += 3
+}
+add3Points()
+add3Points()
+add3Points()
+function remove1Point() {
+    myPoints -= 1
+}
+remove1Point()
+remove1Point()
+console.log(myPoints)
+
+//onlick rendering error message
+
+let error = document.getElementById("error")
+
+function purchase() {
+    error.textContent= "Something went wrong, please try again" 
+}
+
+
+//simple calculator 
+
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let sum = document.getElementById("sum-el") 
+
+function add() {
+    let result = num1 + num2
+    sum.innerText = "Sum: " + result
+}
+
+function subtract() {
+    sum.innerText = num1 - num2
+}
+
+function divide() {
+    sum.innerText = num1 / num2
+}
+
+function multiply() {
+    sum.innerText = num1 * num2
+}
